60c15646084a79bb950e366f628785f597e96d3a
timmy010 <timsh010@gmail.com>
2020-05-05 23:40:07 +0500

    fixed fonts & hover for all links

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,7 @@ gulp.task('sass', function () {
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./src/css'))
 });
-
-// gulp.task('swiper', function() {
-//   return gulp.src('node_modules/swiper/**')
-//     .pipe(gulp.dest('src/swiper/'))
-// })
  
 gulp.task('default', function () {
-  // gulp.watch('./src/scss/**/*.scss', gulp.series( 'sass','swiper'));
   gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
 });
